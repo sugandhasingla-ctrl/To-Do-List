7721a4bf193a2a50a34641bddd82f977063529c0
“Sugandha <sugandha.singla@telusinternational.com>
2025-09-18 18:54:21 +0530

    new changes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,41 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
 export default class ApplicationController extends Controller {
+  newTaskTitle = '';
   tasks = [];
 
-  addTask(title) {
+  @action
+  addTask() {
+    if (!this.newTaskTitle.trim()) {
+      return;
+    }
     const newTask = {
       id: Date.now(),
-      title,
+      title: this.newTaskTitle,
       completed: false
     };
-    this.tasks.push(newTask);
+    this.tasks = [...this.tasks, newTask];
+    this.newTaskTitle = '';
   }
 
-  editTask(taskId, newTitle) {
-    const task = this.tasks.find(task => task.id === taskId);
-    if (task) {
-      task.title = newTitle;
+  @action
+  editTask(taskId, actionType) {
+    if (actionType === "toggleCompletion") {
+      this.tasks = this.tasks.map(task =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      );
     }
+    // You can add more edit logic here (like editing the title)
   }
 
+  @action
   deleteTask(taskId) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  @action
+  updateNewTaskTitle(event) {
+    this.newTaskTitle = event.target.value;
   }
 }
